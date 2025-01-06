b52c14337e5b3f953de38a9ab3abfba7f8777315
Ammar-Abdelhady-ai <ammarabdelhady8@gmail.com>
2025-01-06 07:35:26 +0200

    apply & delete all

---
+++ kubernetes/vpro-app/apply.sh
@@ -0,0 +1,1 @@
+kubectl apply -f "/media/ammar/Volume 2/Devops Project/K8S/vprofile-project/kubernetes/vpro-app/"
---
+++ kubernetes/vpro-app/delete.sh
@@ -0,0 +1,1 @@
+kubectl delete -f "/media/ammar/Volume 2/Devops Project/K8S/vprofile-project/kubernetes/vpro-app/"
